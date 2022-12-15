71fe9bd140a9464f2d7af619e3ffd92faf5897d8
unlomtrois <vadim.kuz02@gmail.com>
2022-12-15 14:29:11 +0300

    add 1

--- ch10-8_python Соболева/$60/1.py
+++ ch10-8_python Соболева/$60/1.py
@@ -3,13 +3,12 @@
 Напишите функцию, которая вычисляет максимальное из трёх чисел, не используя встроенную функцию max.
 """
 
-# вычисляет максимальное из трех чисел
-def my_max(n1, n2, n3):
-    if n1 > n2 and n1 > n3:
-        return n1
-    if n2 > n1 and n2 > n3:
-        return n2
-    return n3
+def max(a, b, c):
+    if a > b and a > c:
+        return a
+    if b > a and b > c:
+        return b
+    return c
 
 
-print(my_max(4, 2, 88))
+print(max(4, 2, 88))
