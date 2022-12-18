71cf7a5b59af95db5cb1691d186ffbd03983f973
unlomtrois <vadim.kuz02@gmail.com>
2022-12-18 18:04:33 +0300

    move 66

---
+++ soboleva-10/$66/9.py
@@ -0,0 +1,14 @@
+"""
+    Напишите функцию, которая определяет, сколько раз входит в символьную строку заданное слово.
+"""
+
+text = input("enter text: ")
+substr = input("enter substring: ")
+count, i = 0, text.find(substr)
+
+while i != -1:
+    count += 1
+    text = text[i + 1 :]
+    i = text.find(substr)
+
+print(count)
