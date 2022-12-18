fa3f2c850662ab68d9f063f4a9a2b18ba254103d
unlomtrois <vadim.kuz02@gmail.com>
2022-12-18 17:25:19 +0300

    3

--- soboleva-10/$63/3.py
+++ soboleva-10/$63/3.py
@@ -1,20 +1,16 @@
 """
-    Найдите за один проход по массиву три его различных элемента, которые меньше всех остальных («три минимума»)
+Соболева ПКС-1
+Найдите за один проход по массиву три его различных элемента, которые меньше всех остальных («три минимума»)
 """
 
 import random
 
-A = []
-for i in range(18):
-    A.append(random.randint(0, 1000))
+arr = [random.randint(0, 1000) for x in range(100)]
 
-min1, min2, min3 = A[0], A[0], A[0]
-# минимальныe значений
-for i in range(1, len(A)):
-    if A[i] < min1:
-        min1, min2, min3 = A[i], min1, min2
+a, b, c = arr[0], arr[0], arr[0]
 
-print(A)
-print("1 min = ", min1)
-print("2 min = ", min2)
-print("3 min = ", min3)
+for i in range(1, len(arr)):
+    if arr[i] < a:
+        a, b, c = arr[i], a, b
+
+print("минимумы:", a, b, c)
