19446c5821810ae28fbe2af8f14e4b4df8915f77
unlomtrois <vadim.kuz02@gmail.com>
2022-12-18 17:39:37 +0300

    1212

--- soboleva-10/$63/10.py
+++ soboleva-10/$63/10.py
@@ -1,24 +1,27 @@
 """
-     ������� � ������� ��� ������� ����� � ���������� �� � ����� ������.
+Соболева ПКС-1
+Найдите в массиве все простые числа и скопируйте их в новый массив.
 """
 
 import random
 
-# ��������� ������� �� ����� ��� ���
-def isPrime(n):
-    k = 2
-    while k * k <= n and n % k != 0:
-        k += 1
 
-    return k * k > n
+def is_prime(num):
+    # предположим, что оно простое
+    isprime = True
+    # чтобы провериь на простоту,
+    # надо проверить на то,
+    # что число не делится ни на что между 2 и числом
+    for n in range(2, num):
+        # если оно делится, то оно не простое
+        if (num % n) == 0:
+            isprime = False
+            break
+
+    return isprime
 
 
-n = 100
-# ���-�� ��������� � ������
-a = [random.randint(1, 1000) for i in range(n)]
+arr = [random.randint(1, 1000) for i in range(100)]
+prime_arr = [num for num in arr if is_prime(num)]
 
-print("������ ��������� �����:\n", a, f" -> {n} ���������\n")
-
-b = [num for num in a if isPrime(num)]
-
-print("���������� ������ ������� �����:\n", b)
+print(prime_arr)
