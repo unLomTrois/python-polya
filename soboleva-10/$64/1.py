1af39f56faaec10f3b547ec0e28694712d89729c
unlomtrois <vadim.kuz02@gmail.com>
2022-12-18 17:43:10 +0300

    1

--- soboleva-10/$64/1.py
+++ soboleva-10/$64/1.py
@@ -3,29 +3,27 @@
 Напишите программу, которая сортирует массив и находит количество различных чисел в нём.
 """
 
-from random import randint
+import random
 
 
-a = [randint(1, 100) for x in range(30)]
-b = []
-
-print("before:", a)
+arr = [random.randint(1, 100) for x in range(100)]
+diff_arr = []
 
 i = 0
-while i < len(a) - 1:
+while i < len(arr) - 1:
     j = i + 1
-    while j < len(a):
-        if a[i] > a[j]:
-            a[i], a[j] = a[j], a[i]
+    while j < len(arr):
+        if arr[i] > arr[j]:
+            arr[i], arr[j] = arr[j], arr[i]
         j += 1
 
     i += 1
 
 i = 0
-while i < len(a) - 1:
-    if not a[i] in b:
-        b.append(a[i])
+while i < len(arr) - 1:
+    if not arr[i] in diff_arr:
+        diff_arr.append(arr[i])
     i += 1
 
-print("after: ", a)
-print("различных чисел:", len(b))
+print(arr)
+print("различных чисел:", len(diff_arr))
